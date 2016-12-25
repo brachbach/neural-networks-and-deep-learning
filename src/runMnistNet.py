5a2994740f733f545368fd290388c2216e7acb0e
Ben Rachbach <ben.rachbach@makewonder.com>
2016-12-24 17:03:48 -0700

    Write py program to run the net

---
+++ src/runMnistNet.py
@@ -0,0 +1,9 @@
+import mnist_loader
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+import network
+
+# [input layer, hidden layers, output layer]
+net = network.Network([784, 10, 10, 10])
+
+# (...epochs,  mini-batch size, learning rate, .. )
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
