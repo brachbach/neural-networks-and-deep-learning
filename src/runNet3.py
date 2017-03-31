a752f16bf364dd38bf7a6287b0c1292c5ca8be8b
Ben Rachbach <ben.rachbach@makewonder.com>
2017-03-30 19:34:12 -0700

    further simplify to a net that's both reasonably fast and reasonably accurate

--- src/runNet3.py
+++ src/runNet3.py
@@ -5,9 +5,9 @@ training_data, validation_data, test_data = network3.load_data_shared()
 mini_batch_size = 10
 net = Network([
         ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28), 
-                      filter_shape=(20, 1, 5, 5), 
+                      filter_shape=(5, 1, 5, 5), 
                       poolsize=(2, 2)),
-        FullyConnectedLayer(n_in=20*12*12, n_out=100),
-        SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
+        FullyConnectedLayer(n_in=5*12*12, n_out=20),
+        SoftmaxLayer(n_in=20, n_out=10)], mini_batch_size)
 net.SGD(training_data, 60, mini_batch_size, 0.1, 
             validation_data, test_data)   
