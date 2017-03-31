badde7405979dba3556f57faecf89d1fc0935d7e
Ben Rachbach <ben.rachbach@makewonder.com>
2017-03-30 19:30:32 -0700

    Use simplest convolutional net from the book, for faster training

--- src/runNet3.py
+++ src/runNet3.py
@@ -1,6 +1,4 @@
 import network3
-expanded_training_data, throwawayvar1, throwawayvar2 = network3.load_data_shared(
-        "../data/mnist_expanded.pkl.gz")
 from network3 import Network
 from network3 import ConvPoolLayer, FullyConnectedLayer, SoftmaxLayer, ReLU
 training_data, validation_data, test_data = network3.load_data_shared()
@@ -8,17 +6,8 @@ mini_batch_size = 10
 net = Network([
         ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28), 
                       filter_shape=(20, 1, 5, 5), 
-                      poolsize=(2, 2), 
-                      activation_fn=ReLU),
-        ConvPoolLayer(image_shape=(mini_batch_size, 20, 12, 12), 
-                      filter_shape=(40, 20, 5, 5), 
-                      poolsize=(2, 2), 
-                      activation_fn=ReLU),
-        FullyConnectedLayer(
-            n_in=40*4*4, n_out=1000, activation_fn=ReLU, p_dropout=0.5),
-        FullyConnectedLayer(
-            n_in=1000, n_out=1000, activation_fn=ReLU, p_dropout=0.5),
-        SoftmaxLayer(n_in=1000, n_out=10, p_dropout=0.5)], 
-        mini_batch_size)
-net.SGD(expanded_training_data, 40, mini_batch_size, 0.03, 
-            validation_data, test_data)
+                      poolsize=(2, 2)),
+        FullyConnectedLayer(n_in=20*12*12, n_out=100),
+        SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
+net.SGD(training_data, 60, mini_batch_size, 0.1, 
+            validation_data, test_data)   
